854cd5ad6f3b909a3cd175349eb2f44b198793e9
glauco.todesco <glauco.todesco@facens.br>
2021-01-15 14:41:19 -0300

    Client PUT, POST and DELETE

--- src/main/java/com/bootcamp/project01/client/resources/ClientResource.java
+++ src/main/java/com/bootcamp/project01/client/resources/ClientResource.java
@@ -1,5 +1,6 @@
 package com.bootcamp.project01.client.resources;
 
+import java.net.URI;
 import com.bootcamp.project01.client.dto.ClientDTO;
 import com.bootcamp.project01.client.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,29 +8,59 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/clients")
 public class ClientResource {
-    
+
     @Autowired
     private ClientService service;
 
     @GetMapping
-    public ResponseEntity <Page<ClientDTO>> getPageOfClients(
-        @RequestParam(value = "page", defaultValue = "0") Integer page,
-        @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-        @RequestParam(value = "direction", defaultValue = "ASC") String direction,
-        @RequestParam(value = "orderBy", defaultValue = "name") String orderBy	
-    ){
+    public ResponseEntity<Page<ClientDTO>> getPageOfClients(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
 
-        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
-		Page<ClientDTO> dtoPage = service.getPageOfClients(pageRequest);
-		return ResponseEntity.ok().body(dtoPage);
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        Page<ClientDTO> dtoPage = service.getPageOfClients(pageRequest);
+        return ResponseEntity.ok().body(dtoPage);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ClientDTO> getClientById(@PathVariable Long id) {
+        ClientDTO dto = service.getClientById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> create(@RequestBody ClientDTO dto) {
+        dto = service.create(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok().body(dto);
     }
 
 }
--- src/main/java/com/bootcamp/project01/client/services/ClientService.java
+++ src/main/java/com/bootcamp/project01/client/services/ClientService.java
@@ -1,13 +1,21 @@
 package com.bootcamp.project01.client.services;
 
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
 import com.bootcamp.project01.client.dto.ClientDTO;
 import com.bootcamp.project01.client.entities.Client;
 import com.bootcamp.project01.client.repositories.ClientRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class ClientService {
@@ -15,10 +23,54 @@ public class ClientService {
     @Autowired
     private ClientRepository repository;
 
+    @Transactional(readOnly = true)
     public Page<ClientDTO> getPageOfClients(PageRequest pageRequest){
         Page<Client> page = repository.findAll(pageRequest);
         return page.map( c -> new ClientDTO(c));
     }
 
+    @Transactional(readOnly = true)
+	public ClientDTO getClientById(Long id) {
+        Optional<Client> op = repository.findById(id);
+        Client client = op.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Id not found"));
+        return new ClientDTO(client);
+    }
+    
+    @Transactional
+    public void delete(Long id){
+        try{
+            repository.deleteById(id);
+        }
+        catch(EmptyResultDataAccessException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Id not found");
+        }
+    }
+
+    @Transactional
+	public ClientDTO create(ClientDTO dto) {
+        dto.setId(null);
+        Client client = repository.save(new Client(dto));
+        return new ClientDTO(client);
+    }
+    
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        try {
+            Client entity = repository.getOne(id);
+            
+            entity.setName(dto.getName());
+            entity.setBirthDate(dto.getBirthDate());
+            entity.setCpf(dto.getCpf());
+            entity.setIncome(dto.getIncome());
+            entity.setChildren(dto.getChildren());
+            
+            repository.save(entity);
+
+            return new ClientDTO(entity);
+        }
+        catch(EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Id not found " + id);
+        }
+    }
 
 }
